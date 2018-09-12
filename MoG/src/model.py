211f951367173d0d27387c6e7b0680a569a08e42
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-09-12 00:41:42 -0500

    initial commit

---
+++ MoG/src/model.py
@@ -0,0 +1,11 @@
+"""Develop a model for the probability distribution.
+
+Model the change in the best ask size (or best bid size).
+Implement mixture of Gaussians where the coefficients for the mixture are
+outputs of a neural network and where the means/variances of the Gaussians
+are also outputs of a neural network.
+
+@author Zhenye Na
+
+created on Jun, 2018
+"""
